eac47ffd91d93c1f35e4e43b26307914641bf5d3
Jacky <adens123@gmail.com>
2024-04-16 17:34:10 +0800

    Finished the live chat feature.

--- app/our-history/page.jsx
+++ app/our-history/page.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function OurHistory() {
   return (
@@ -10,11 +10,30 @@ export default function OurHistory() {
 
         <h2 className="page-section-title">Our History</h2>
         <div className="generic-page-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem sequi neque, nostrum sapiente quas quasi ut maxime repellendus nemo vitae esse itaque illum deleniti officia, aliquam fuga. Quibusdam, dolores.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos recusandae dolorum repellendus laudantium itaque ipsam inventore magnam illo nemo fugit rem, officiis omnis soluta repudiandae, quod, sint praesentium. Ratione, rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi beatae exercitationem quibusdam maiores repudiandae autem quisquam asperiores consequuntur eum natus ipsam in, praesentium adipisci dicta nobis esse accusantium! Nostrum, necessitatibus.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In aperiam deleniti iusto dolore similique cum quos vitae est repellendus architecto quod culpa fugiat ad, hic distinctio quisquam assumenda reiciendis laborum.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem
+            sequi neque, nostrum sapiente quas quasi ut maxime repellendus nemo
+            vitae esse itaque illum deleniti officia, aliquam fuga. Quibusdam,
+            dolores.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            recusandae dolorum repellendus laudantium itaque ipsam inventore
+            magnam illo nemo fugit rem, officiis omnis soluta repudiandae, quod,
+            sint praesentium. Ratione, rem? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Animi beatae exercitationem quibusdam
+            maiores repudiandae autem quisquam asperiores consequuntur eum natus
+            ipsam in, praesentium adipisci dicta nobis esse accusantium!
+            Nostrum, necessitatibus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In aperiam
+            deleniti iusto dolore similique cum quos vitae est repellendus
+            architecto quod culpa fugiat ad, hic distinctio quisquam assumenda
+            reiciendis laborum.
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
